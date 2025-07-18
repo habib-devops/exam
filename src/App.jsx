39ad422b2bdd7a83f5f5dbd1116d624326813d5d
“[habib-devops]” <[habibul-04devops@dipti.com.bd]>
2025-07-18 16:05:21 +0000

    Initial demo app with my name on homepage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, I'm Habibul Hasan!</h1>
+      <p>This is a React demo running locally.</p>
+    </div>
+  );
+}
+
+export default App;
